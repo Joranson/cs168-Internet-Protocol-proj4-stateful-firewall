def3f852892c411e583851bc3aef6ae3fa36dc44
Qiyin Wu <joranson.wu@berkeley.edu>
2015-12-02 01:15:41 -0800

    first attempt of http log; need to implement two methods

--- firewall.py
+++ firewall.py
@@ -5,7 +5,7 @@ from main import PKT_DIR_INCOMING, PKT_DIR_OUTGOING
 import struct
 import socket
 
-# TODO: Feel free to import any Python standard moduless as necessary.
+
 # (http://docs.python.org/2/library/)
 # You must NOT use any 3rd-party libraries.
 
@@ -42,11 +42,21 @@ class Firewall:
                 print i
                 print "Initialization finished"
                 
-        # # Load the GeoIP DB  --  no GeoIP for Project 4. //TODO: clean everything related to GeoIp
+        ## Load the GeoIP DB  --  no GeoIP for Project 4. //TODO: clean everything related to GeoIp
         self.geoDb = []
         with open('geoipdb.txt') as f:
             self.geoDb = f.readlines()
-        self.geoDb = []
+
+        ## create a dictionary to store {unique_id: http_payload} for reassembly TCP
+        self.reassembly = {}
+        ## create a dictionary to store {unique_id: expected_seq} for reassembly TCP
+        self.expected_seq = {}
+        ## CRLF, which is "\r\n\r\n"
+        self.crlf = (struct.pack("!B",13)+struct.pack("!B",10))*2
+        ## tracking content-length to clean self.reassembly once receive the header
+        self.contentLength = {}
+        # temporary http_request_info backup
+        self.http_request_info = {}
 
     def create_ip_deny_packet_header(self, source_addr, dest_addr, ip_protocol, total_length):
         if self.debug:
@@ -184,7 +194,58 @@ class Firewall:
                     print "source port is", source_port
                     print "destination port is", dest_port
 
-                if pkt_dir == PKT_DIR_INCOMING:
+                if (source_port==80 and pkt_dir==PKT_DIR_INCOMING) or (dest_port==80 and pkt_dir==PKT_DIR_OUTGOING):    ## HTTP connection
+                    tcp_header_len = struct.unpack("!B", pkt[ip_header_len+12])[0]>>4 *4  ## tcp header size in Byte
+                    ip_total_len = struct.unpack("!H", pkt[2:4])[0]
+                    if ip_total_len>ip_header_len+tcp_header_len:  ## there is payload related to HTTP connection
+                        http_payload_len = ip_total_len-ip_header_len-tcp_header_len
+                        http_payload = pkt[ip_header_len+tcp_header_len:]
+                        seq_num = struct.unpack("!L",pkt[ip_header_len+4:ip_header_len+8])[0]
+                        expected_next_seq_num = seq_num+http_payload_len
+                        unique_id = (source_addr, dest_addr, source_port, dest_port)
+                        if unique_id in self.reassembly:
+                            if seq_num<=self.expected_seq[unique_id]:   # ignore forward out-of-order cases
+                                self.reassembly[unique_id]+=http_payload
+                                self.expected_seq[unique_id] = expected_next_seq_num
+                                if self.crlf in self.reassembly[unique_id]:  # already parsed http header
+                                    retrieveInfo = self.retrieveInfo(self.reassembly[unique_id])    ##TODO: need to implement this method
+                                    self.contentLength[unique_id] = retrieveInfo["content-length"] # if no content-length field, set it to -1
+                                    if pkt_dir==PKT_DIR_OUTGOING:   # http request
+                                        self.http_request_info[unique_id] = retrieveInfo
+                                    else:                           # http response
+                                        # retrieve http request info
+                                        reverse_unique_id = (unique_id[1],unique_id[0], unique_id[3], unique_id[2])
+                                        http_request_info = self.http_request_info[reverse_unique_id]
+                                        self.log(http_request_info, retrieveInfo) ##TODO: need to implement this method
+                                if unique_id not in self.contentLength:
+                                    self.contentLength[unique_id] = float("inf") # set it to be infinity, waiting retrieveInfo to update itself
+
+                                if self.contentLength[unique_id]<=len(self.reassembly[unique_id]):
+                                    self.reassembly[unique_id]=""       # reset so next time search crlf split would also work for one of the next response/request pair
+                                    self.contentLength[unique_id] = float("inf")
+                        else:
+                            if seq_num==1:      # after handshake since handshake stages have no payload
+                                self.reassembly[unique_id] = http_payload
+                                self.expected_seq[unique_id] = expected_next_seq_num
+                                if self.crlf in self.reassembly[unique_id]:  # already parsed http header
+                                    retrieveInfo = self.retrieveInfo(self.reassembly[unique_id])
+                                    self.contentLength[unique_id] = retrieveInfo["content-length"] # if no content-length field, set it to -1
+                                    if pkt_dir==PKT_DIR_OUTGOING:   # http request
+                                        self.http_request_info[unique_id] = retrieveInfo
+                                    else:                           # http response
+                                        # retrieve http request info
+                                        reverse_unique_id = (unique_id[1],unique_id[0], unique_id[3], unique_id[2])
+                                        http_request_info = self.http_request_info[reverse_unique_id]
+                                        self.log(http_request_info, retrieveInfo) ##TODO: need to implement this method
+
+                                if unique_id not in self.contentLength:
+                                    self.contentLength[unique_id] = float("inf") # set it to be infinity, waiting retrieveInfo to update itself
+
+                                if self.contentLength[unique_id]<=len(self.reassembly[unique_id]):
+                                    self.reassembly[unique_id]=""       # reset so next time search crlf split would also work for one of the next response/request pair
+                                    self.contentLength[unique_id] = float("inf")
+
+                elif pkt_dir == PKT_DIR_INCOMING:
                     if self.debug:
                         print "incoming packet"
                     pkt_info['external_port'] = source_port
@@ -574,6 +635,17 @@ class Firewall:
                     dnsName = dnsName+"."       # adding dot
                 j+=1
         return dnsName.lower(), j
+
+    def retrieveInfo(self, payload):
+        """
+        :rtype: a dictionary specifying host_name, method, path, version, status_code, object_size
+        """
+        pass
+
+    def log(self, http_request_info, http_response_info):
+        pass
+
+
 
 
 
